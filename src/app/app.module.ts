e2dd4208215866552a19646953265b9e8e20322e
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-02-03 19:20:05 -0500

    Octava app en Angular - CRUD Firebase, configuracion del proyecto , estilos y creacion de dos componentes , heroes y heroe y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { AAP_ROUTING } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AAP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
